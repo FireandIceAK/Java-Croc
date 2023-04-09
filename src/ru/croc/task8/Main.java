40ae76c3ad476b480b46cd12e92bdfc62529e52a
Крылов Анатолий <tolya_krylov_2013@mail.ru>
2023-04-09 19:16:29 +0500

    Решение восьмой задачи

--- src/ru/croc/task8/Main.java
+++ src/ru/croc/task8/Main.java
@@ -1,7 +1,25 @@
 package ru.croc.task8;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.out.println("Usage: java WordCount <filename>");
+            return;
+        }
+
+        int wordCount = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.trim().split("\\s+");
+                wordCount += words.length;
+            }
+        }
+
+        System.out.println("Слов в тексте: " + wordCount);
     }
 }
